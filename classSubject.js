07b6ab0b59cc4f2e18708922ad4372e588b1d439
kulotskiI <ishielmishma24@gmail.com>
2024-11-04 19:20:32 +0800

    initial commit

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let Subject = "DSA";
+let classSchedule = "Tuesday and Thursday ,1:00 - 2:00 pm";
+let classroom = "Comlab A";
+let classInstructor = " Dr.Vitruvis John Barayga";
+let studentName = "Ishiel Mishma Ganaban";
+
+console.log(`${studentName} is currently enrolled in ${Subject} with a class schedule of ${classSchedule} at ${classroom}. The instructor for this subject is ${classInstructor}.`);
